300118fd1c11bd892d3a660e916ea663dd7a1eed
Chase Bradley <bradleycha@proton.me>
2024-04-13 12:49:47 -0400

    Fix build script for multiple files

--- build.sh
+++ build.sh
@@ -6,11 +6,10 @@ PATH_BIN=$PATH_ROOT/bin
 PATH_INT=$PATH_BIN/int
 FILE_JAR=$PATH_BIN/encrypt.jar
 FILE_MANIFEST=$PATH_SRC/Manifest.mf
-FILE_MAIN=$PATH_SRC/Main.java
 
 # This is a horribly inflexible build solution, but I'm a noob to real-world
 # Java development and the existing solutions are either 20 years obsolete or
 # are way overkill for this simple program.
-javac $FILE_MAIN -d $PATH_INT
+javac $PATH_SRC/*.java -d $PATH_INT
 jar cvfm $FILE_JAR $FILE_MANIFEST $PATH_INT/*
 
